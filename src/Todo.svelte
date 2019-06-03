f17bf6dd772c917e8de63f6754f6d8db028ec17e
Chris Williams <christophw+github@protonmail.com>
2019-06-03 15:34:42 +0100

    todo comp

---
+++ src/Todo.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let id;
+  export let name;
+  export let description;
+  export let complete;
+
+  import { Firestore } from "./firebase";
+
+  function toggleHandler() {
+    const ref = Firestore.doc(`todos/${id}`);
+    ref.update({ complete: !complete });
+  }
+
+  function deleteHandler() {
+    const ref = Firestore.doc(`todos/${id}`);
+    ref.delete();
+  }
+</script>
+
+<style>
+  .complete {
+    text-decoration: line-through;
+    color: crimson;
+  }
+</style>
+
+<h3 class:complete>{name}</h3>
+<p>{description}</p>
+
+<button on:click={toggleHandler} class="button">
+  Mark {complete ? 'Incomplete' : 'Complete'}
+</button>
+
+<button on:click={deleteHandler} class="button">Delete</button>
