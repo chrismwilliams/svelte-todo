5214026e970be549082fab1bdac57c14c8b173e5
Chris Williams <christophw+github@protonmail.com>
2019-06-03 15:34:26 +0100

    header comp

---
+++ src/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let user;
+
+  import { getContext } from "svelte";
+  import { Auth } from "./firebase";
+
+  const { setUser } = getContext("user");
+
+  async function logout() {
+    Auth.signOut();
+    setUser(null);
+  }
+</script>
+
+<style>
+
+</style>
+
+<header>
+  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <p class="navbar-item">Svelte todo app</p>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-start" />
+      <div class="nav-bar-end">
+        {#if user}
+          <button on:click={logout} class="button navbar-item">Log out</button>
+        {/if}
+      </div>
+    </div>
+  </nav>
+</header>
