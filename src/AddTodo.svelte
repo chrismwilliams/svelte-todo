6b08ee37f3da77a6f42f8ba353281b275ba2ce32
Chris Williams <christophw+github@protonmail.com>
2019-06-04 11:35:45 +0100

    comp for adding a todo

---
+++ src/AddTodo.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+
+  let task = "";
+
+  function addTodo() {
+    if (!task || task == "") return;
+    dispatch("add", {
+      task
+    });
+    task = "";
+  }
+</script>
+
+<style>
+  .container {
+    margin-top: 3rem;
+  }
+</style>
+
+<div class="container">
+  <h2 class="title">Add Todo</h2>
+  <form id="task-form" on:submit|preventDefault={addTodo}>
+    <div class="field">
+      <label class="label" for="todo">New task</label>
+      <div class="control">
+        <input
+          class="input"
+          id="todo"
+          type="text"
+          placeholder="New task"
+          bind:value={task}
+          required />
+      </div>
+    </div>
+    <div class="field is-grouped">
+      <div class="control">
+        <button
+          class="button is-primary has-text-weight-semibold"
+          type="submit">
+          Add Todo
+        </button>
+      </div>
+    </div>
+  </form>
+</div>
